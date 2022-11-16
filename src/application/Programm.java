db4349465644cf1cf1bbcd782f913fcd1ea2f6e3
lucasn07 <lucasn0796@gmail.com>
2022-11-16 00:16:30 -0300

    Reutilizando a instanciação da classe -> SellerDaoJDBC

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -10,9 +10,10 @@ public class Programm {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
 
 	}
 
